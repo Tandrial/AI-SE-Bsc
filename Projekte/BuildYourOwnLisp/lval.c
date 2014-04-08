08877ee523b6af2d4147dc71997fb6080edd17c6
Michael  Krane <Michael.Krane@gmx.de>
2014-04-08 23:41:51 +0200

    mem leak fix

--- Projekte/BuildYourOwnLisp/lval.c
+++ Projekte/BuildYourOwnLisp/lval.c
@@ -248,7 +248,11 @@ int lval_eq(lval* x, lval* y) {
 
 // ---- Verarbeitet den Type Eval
 lval* lval_eval(lenv* e, lval* v) {
-	if (v->type == LVAL_SYM)   { return lenv_get(e, v); }
+	if (v->type == LVAL_SYM) {
+		lval* x = lenv_get(e, v);
+		lval_del(v);
+		return x;
+	}
 	if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(e, v); }
 	return v;
 }

81f3e1b4b3f32a4208fec0b0d58bd3d13963dc1f
Michael Krane <Michael.Krane@gmx.de>
2015-06-17 19:23:10 +0200

    now?

--- src/uni/dc/ubsOpti/Tracer/Traceable.java
+++
@@ -1,9 +0,0 @@
-package uni.dc.ubsOpti.tracer;
-
-import uni.dc.ubsOpti.UbsOptiConfig;
-
-public interface Traceable {
-	public void setUpTrace(UbsOptiConfig config);
-
-	public DelayTrace getTrace();
-}

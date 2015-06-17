d9bc1b5f96720640031e8340cc4e366eaf77cadb
Michael Krane <Michael.Krane@gmx.de>
2015-06-17 19:25:20 +0200

    Revert "now?"
    This reverts commit 81f3e1b4b3f32a4208fec0b0d58bd3d13963dc1f.

---
+++ src/uni/dc/ubsOpti/DelayCalc/UbsDelayCalc.java
@@ -0,0 +1,136 @@
+package uni.dc.ubsOpti.delayCalc;
+
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Set;
+
+import org.goataa.impl.OptimizationModule;
+import org.goataa.spec.IObjectiveFunction;
+
+import uni.dc.model.EgressPort;
+import uni.dc.model.Flow;
+import uni.dc.model.PriorityConfiguration;
+
+public abstract class UbsDelayCalc extends OptimizationModule implements IObjectiveFunction<int[]>, Serializable {
+	private static final long serialVersionUID = 1L;
+	protected Set<Flow> flows = null;
+	protected PriorityConfiguration prio = null;
+	private double fitness = 0.0d;
+
+	public PriorityConfiguration getPrio() {
+		return prio;
+	}
+
+	public double getFitness() {
+		return fitness;
+	}
+
+	public void setPrio(PriorityConfiguration prio) {
+		this.prio = prio;
+	}
+
+	public UbsDelayCalc(Map<EgressPort, Set<Flow>> traffic) {
+		flows = new LinkedHashSet<Flow>();
+		for (Entry<EgressPort, Set<Flow>> x : traffic.entrySet()) {
+			for (Flow bla : x.getValue()) {
+				flows.add(bla);
+			}
+		}
+	}
+
+	public UbsDelayCalc(Set<Flow> flows) {
+		this.flows = flows;
+	}
+
+	public void calculateDelays(PriorityConfiguration prio) {
+		this.prio = prio;
+		calculateDelays();
+	}
+
+	public void calculateDelays(int[] prios) {
+		prio.fromIntArray(prios);
+		calculateDelays();
+	}
+
+	public abstract void calculateDelays();
+
+	protected double calcDelay(EgressPort lastEgress, Flow f) {
+		double delay;
+		double sizeBiggerEq = 0.0;
+		double maxSmaller = 0.0;
+		double rateHigher = 0.0;
+
+		double linkSpeed = lastEgress.getLinkSpeed();
+		double size = f.getMaxFrameLength();
+		int prioF = prio.getPriority(lastEgress, f);
+
+		for (Flow other : lastEgress.getFlowList()) {
+			if (f == other)
+				continue;
+			int prioOther = prio.getPriority(lastEgress, other);
+			if (prioOther < prioF) {
+				sizeBiggerEq += other.getMaxFrameLength();
+				rateHigher += other.getRate();
+			} else if (prioOther == prioF) {
+				sizeBiggerEq += other.getMaxFrameLength();
+			} else {
+				maxSmaller = Math.max(maxSmaller, other.getMaxFrameLength());
+			}
+		}
+		delay = (sizeBiggerEq + maxSmaller) / (linkSpeed - rateHigher) + size / linkSpeed;
+		return delay;
+	}
+
+	public void setInitialDelays(PriorityConfiguration prio) {
+		this.prio = prio;
+		calculateDelays();
+		for (Flow f : flows) {
+			f.setInitialMaxLatencyRequirement();
+		}
+	}
+
+	@Override
+	public String toString(boolean longVersion) {
+		StringBuilder sb = new StringBuilder();
+		for (Flow flow : flows) {
+			sb.append(String.format("%s : %.0f Mbps, %d bit\n", flow.getName(), flow.getRate() / 1e6,
+					flow.getMaxFrameLength()));
+			sb.append(String.format("Path : %s\n", flow));
+			if (flow.getMaxLatency() == Double.MAX_VALUE) {
+				sb.append(String.format("Destination %s delay is %.3e s\n", flow.getDestPort(), flow.getDelay()));
+			} else {
+				sb.append(String.format("Destination %s maxLat of %.3e s, delay is %.3e s\n", flow.getDestPort(),
+						flow.getMaxLatency(), flow.getDelay()));
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public boolean checkDelays() {
+		for (Flow flow : flows) {
+			if (!flow.checkDelay())
+				return false;
+		}
+		return true;
+	}
+
+	@Override
+	public double compute(int[] x, Random r) {
+		prio.fromIntArray(x);
+		calculateDelays();
+		double delay = 0.0d;
+		for (Flow f : flows) {
+			delay += f.getDelay();
+			if (!f.checkDelay()) {
+				// TODO: Strafe für Delay > maxLatencyReq
+				delay += 1;
+			}
+		}
+		fitness = delay;
+		return delay;
+	}
+}
---
+++ src/uni/dc/ubsOpti/DelayCalc/UbsV0DelayCalc.java
@@ -0,0 +1,32 @@
+package uni.dc.ubsOpti.delayCalc;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import uni.dc.model.EgressPort;
+import uni.dc.model.Flow;
+
+public class UbsV0DelayCalc extends UbsDelayCalc {
+	private static final long serialVersionUID = 1L;
+
+	public UbsV0DelayCalc(Map<EgressPort, Set<Flow>> traffic) {
+		super(traffic);
+	}
+
+	public UbsV0DelayCalc(Set<Flow> traffic) {
+		super(traffic);
+	}
+
+	@Override
+	public void calculateDelays() {
+		for (Flow f : flows) {
+			List<EgressPort> path = f.getPath();
+			double delay = 0.0;
+			for (int i = 1; i < path.size(); i++) {
+				delay += calcDelay(path.get(i - 1), f);
+			}
+			f.setDelay(delay);
+		}
+	}
+}

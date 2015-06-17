14ad75a9aece835968e5efae7c629a81f932010a
Michael Krane <michael.krane@gmx.de>
2015-06-17 19:49:04 +0200

    Linux UI fixes

--- src/uni/dc/view/SettingsGui.java
+++ src/uni/dc/view/SettingsGui.java
@@ -41,8 +41,9 @@ public class SettingsGui extends JDialog {
 	private JRadioButton rdbtnUbsV3;
 
 	public SettingsGui(UbsOptiConfig config) {
+		setResizable(false);
 		setTitle("Settings - UBsOpti");
-		setBounds(100, 100, 195, 325);
+		setBounds(100, 100, 230, 325);
 		getContentPane().setLayout(new BorderLayout());
 		JPanel contentPanel = new JPanel();
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -50,73 +51,73 @@ public class SettingsGui extends JDialog {
 		contentPanel.setLayout(null);
 
 		JLabel lblNetworkDepth = new JLabel("Depth");
-		lblNetworkDepth.setBounds(10, 25, 75, 15);
+		lblNetworkDepth.setBounds(10, 25, 100, 15);
 		contentPanel.add(lblNetworkDepth);
 
 		JLabel lblPortCount = new JLabel("Port Count");
-		lblPortCount.setBounds(10, 50, 75, 15);
+		lblPortCount.setBounds(10, 50, 100, 15);
 		contentPanel.add(lblPortCount);
 
-		JLabel lblMaxframelength = new JLabel("maxLength[bit]");
-		lblMaxframelength.setBounds(10, 75, 75, 15);
+		JLabel lblMaxframelength = new JLabel("maxLength");
+		lblMaxframelength.setBounds(10, 75, 100, 15);
 		contentPanel.add(lblMaxframelength);
 
 		JLabel lblMaxspeed = new JLabel("maxSpeed[%]");
-		lblMaxspeed.setBounds(10, 100, 75, 15);
+		lblMaxspeed.setBounds(10, 100, 100, 15);
 		contentPanel.add(lblMaxspeed);
 
 		JLabel lblMaxprio = new JLabel("maxPrio");
-		lblMaxprio.setBounds(10, 155, 75, 15);
+		lblMaxprio.setBounds(10, 155, 100, 15);
 		contentPanel.add(lblMaxprio);
 
 		JLabel lblMaxsteps = new JLabel("maxSteps");
-		lblMaxsteps.setBounds(10, 180, 75, 15);
+		lblMaxsteps.setBounds(10, 180, 100, 15);
 		contentPanel.add(lblMaxsteps);
 
 		JLabel lblRuns = new JLabel("runs");
-		lblRuns.setBounds(10, 205, 75, 15);
+		lblRuns.setBounds(10, 205, 100, 15);
 		contentPanel.add(lblRuns);
 
 		spDepth = new JSpinner();
 		spDepth.setModel(new SpinnerNumberModel(new Integer(2), new Integer(2), null, new Integer(1)));
 		spDepth.setValue(config.getDepth());
-		spDepth.setBounds(85, 25, 80, 20);
+		spDepth.setBounds(125, 25, 80, 20);
 		contentPanel.add(spDepth);
 
 		spPortCount = new JSpinner();
 		spPortCount.setModel(new SpinnerNumberModel(new Integer(2), new Integer(2), null, new Integer(1)));
 		spPortCount.setValue(config.getPortCount());
-		spPortCount.setBounds(85, 50, 80, 20);
+		spPortCount.setBounds(125, 50, 80, 20);
 		contentPanel.add(spPortCount);
 
 		spMaxLength = new JSpinner();
 		spMaxLength.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		spMaxLength.setValue(config.getMaxFrameLength());
-		spMaxLength.setBounds(85, 74, 80, 20);
+		spMaxLength.setBounds(125, 74, 80, 20);
 		contentPanel.add(spMaxLength);
 
 		spSpeed = new JSpinner();
 		spSpeed.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), new Integer(100), new Integer(1)));
 		spSpeed.setValue(config.getMaxSpeed());
-		spSpeed.setBounds(85, 101, 80, 20);
+		spSpeed.setBounds(125, 101, 80, 20);
 		contentPanel.add(spSpeed);
 
 		spMaxPrio = new JSpinner();
 		spMaxPrio.setModel(new SpinnerNumberModel(new Integer(2), new Integer(2), null, new Integer(1)));
 		spMaxPrio.setValue(config.getMaxPrio());
-		spMaxPrio.setBounds(85, 150, 80, 20);
+		spMaxPrio.setBounds(125, 150, 80, 20);
 		contentPanel.add(spMaxPrio);
 
 		spMaxStep = new JSpinner();
 		spMaxStep.setModel(new SpinnerNumberModel(new Integer(10000), new Integer(10000), null, new Integer(1)));
 		spMaxStep.setValue(config.getMaxSteps());
-		spMaxStep.setBounds(85, 175, 80, 20);
+		spMaxStep.setBounds(125, 175, 80, 20);
 		contentPanel.add(spMaxStep);
 
 		spRuns = new JSpinner();
 		spRuns.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		spRuns.setValue(config.getRuns());
-		spRuns.setBounds(85, 200, 80, 20);
+		spRuns.setBounds(125, 200, 80, 20);
 		contentPanel.add(spRuns);
 
 		rdbtnUbsV0 = new JRadioButton("UBS V0");
@@ -124,7 +125,7 @@ public class SettingsGui extends JDialog {
 		contentPanel.add(rdbtnUbsV0);
 
 		rdbtnUbsV3 = new JRadioButton("UBS V3");
-		rdbtnUbsV3.setBounds(85, 235, 80, 20);
+		rdbtnUbsV3.setBounds(105, 235, 100, 20);
 		contentPanel.add(rdbtnUbsV3);
 
 		if (config.isUbsV0()) {
@@ -142,13 +143,13 @@ public class SettingsGui extends JDialog {
 		JLabel lblGeneratorConfig = new JLabel("Generator Config");
 		lblGeneratorConfig.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGeneratorConfig.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblGeneratorConfig.setBounds(10, 0, 155, 20);
+		lblGeneratorConfig.setBounds(10, 0, 195, 20);
 		contentPanel.add(lblGeneratorConfig);
 
 		JLabel lblOptimizerConfig = new JLabel("Optimizer Config");
 		lblOptimizerConfig.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOptimizerConfig.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblOptimizerConfig.setBounds(10, 124, 155, 20);
+		lblOptimizerConfig.setBounds(10, 124, 195, 20);
 		contentPanel.add(lblOptimizerConfig);
 
 		JPanel buttonPane = new JPanel();

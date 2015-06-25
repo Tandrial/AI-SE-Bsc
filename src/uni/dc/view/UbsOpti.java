65579c3deda6ddd390b2b1b751d36a28c3bc6437
Michael Krane <michael.krane@gmx.de>
2015-06-25 21:45:11 +0200

    BruteForce removed

--- src/uni/dc/view/UbsOpti.java
+++ src/uni/dc/view/UbsOpti.java
@@ -22,6 +22,9 @@ import uni.dc.ubsOpti.tracer.EndStepTracer;
 
 public class UbsOpti {
 
+	@Option(name = "-factor", usage = "Factor that makes the optimazation easier")
+	private double factor = 1.2d;
+
 	@Option(name = "-minPort", usage = "min amount of Ports")
 	private int minPort = 2;
 
@@ -43,27 +46,27 @@ public class UbsOpti {
 	@Option(name = "-runs", usage = "runs for each Configuration")
 	private int runs = 50;
 
-	@Option(name = "-BF", usage = "BruteForce enabled")
-	private boolean BF = false;
-
 	@Option(name = "-BT", usage = "BackTrack enabled")
-	private boolean BT = true;
+	private boolean BT = false;
 
 	@Option(name = "-HC", usage = "HillClimbing enabled")
-	private boolean HC = true;
+	private boolean HC = false;
 
 	@Option(name = "-SA", usage = "Simulated Annealing enabled")
-	private boolean SA = true;
+	private boolean SA = false;
 
 	@Option(name = "-sEA", usage = "simple EvolutionAlgo enabled")
-	private boolean sEA = true;;
+	private boolean sEA = false;
 
 	@Argument
 	private List<String> arguments = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		args = new String[1];
+		args = new String[4];
 		args[0] = "-BT";
+		args[1] = "-HC";
+		args[2] = "-SA";
+		args[3] = "-sEA";
 
 		if (args.length == 0) {
 			new UbsOpti().startGui();
@@ -90,22 +93,24 @@ public class UbsOpti {
 		Optimizer opti = Optimizer.getOptimizer();
 		UbsOptiConfig config = new UbsOptiConfig();
 		config.setSeed(minPort << 1 + maxPort << 2 + minFlow << 3 + maxFlow << 4 + minPrio << 5 + maxPrio << 6);
+		config.setModifier(factor);
 		EndStepTracer tracer = config.newEndStepTracer();
 		opti.addTracer(tracer);
 		long t1, t2;
 		t1 = System.nanoTime();
 		for (int portCount = minPort; portCount <= maxPort; portCount++) {
 			config.setPortCount(portCount);
-			for (int depthCount = 2; depthCount <= portCount; depthCount++) {
+			for (int depthCount = 2; depthCount <= portCount - 2; depthCount++) {
 				config.setDepth(depthCount);
 				config.newTopology();
 				System.out.println(String.format("\nNew Topologies : %d ports, %d depth:", portCount, depthCount));
-				for (int flowCount = minFlow; flowCount <= maxFlow; flowCount++) {
+				for (int flowCount = minFlow; flowCount <= Math.min(maxFlow, portCount - 2); flowCount++) {
 					config.setFlowCount(flowCount);
 					System.out.println(String.format("  flowCount : %d", flowCount));
 					for (int prioCount = minPrio; prioCount <= maxPrio; prioCount++) {
 						config.setMaxPrio(prioCount);
-						System.out.print(String.format("    prioCount : %d\n      ", prioCount));
+						System.out.print(String.format("    maxStep : %d    prioCount : %d\n      ",
+								config.getMaxSteps(), prioCount));
 						int cnt = 0;
 						for (int run = 1; run <= runs; run++) {
 							String id = String.format("%03d%03d%03d%03d", portCount, depthCount, flowCount, prioCount,
@@ -115,8 +120,6 @@ public class UbsOpti {
 								System.out.print("..." + cnt);
 							config.newTraffic();
 							config.setPriorityConfig(new PriorityConfiguration(config.getTraffic()));
-							if (portCount <= 10 && BF)
-								opti.optimize(config, "BF");
 							if (BT)
 								opti.optimize(config, "BT");
 							if (HC)
@@ -135,7 +138,7 @@ public class UbsOpti {
 		t2 = System.nanoTime();
 		String fileName = String.format("%d_%dports_%dStreamConfigCount_%dmaxSteps_%dMaxPrio_%dmodi%s%s%s%s%s.csv",
 				minPort, maxPort, runs, config.getMaxSteps(), config.getMaxPrio(), (int) (config.getModifier() * 100),
-				BF ? "BF" : "", BT ? "BT" : "", HC ? "HC" : "", SA ? "SA" : "", sEA ? "sEA" : "");
+				BT ? "BT" : "", HC ? "HC" : "", SA ? "SA" : "", sEA ? "sEA" : "");
 		EndStepTracer.saveToFile(new File("./Traces/" + fileName), tracer);
 		System.out
 				.println(String.format("Batch done! (took %.4f s)\n See %s for results!", (t2 - t1) / 1.0e9, fileName));

51f1947b6b6ed4bb90a24795312b455dbd6d1fcf
Michael Krane <michael.krane@gmx.de>
2015-09-22 20:39:00 +0200

    Matasano Challenge progrss - Set 1 4/8

--- Projekte/CryptoPals/src/set1/Challenge1.java
+++ Projekte/CryptoPals/src/set1/Challenge1.java
@@ -3,8 +3,25 @@ package set1;
 import java.util.Base64;
 
 public class Challenge1 {
+
+	public static byte[] hexToInt(byte[] bytes) {
+		byte[] res = new byte[bytes.length / 2];
+		for (int i = 0; i < bytes.length / 2; i++) {
+			res[i] = (byte) (hexToDec(bytes[i * 2]) * 16 + hexToDec(bytes[i * 2 + 1]));
+		}
+		return res;
+	}
+
+	public static int hexToDec(byte c) {
+		if ('0' <= c && '9' >= c) {
+			return c - '0';
+		} else {
+			return c - 'a' + 10;
+		}
+	}
+
 	public static String hexToBase64(String s) {
-		return Base64.getEncoder().encodeToString(s.getBytes());
+		return Base64.getEncoder().encodeToString(hexToInt(s.getBytes()));
 	}
 
 	public static void main(String[] args) {
@@ -12,7 +29,6 @@ public class Challenge1 {
 		String out = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 		String myOut = hexToBase64(in);
 		System.out.println(
-				String.format("in    : %s \nmyOut : %s \nout   : %s \nequal? %s", in, myOut, out, myOut.equals(out)));
+				String.format("in    : %s \nmyOut : %s \nout   : %s \nequals? %s", in, myOut, out, myOut.equals(out)));
 	}
-
 }
---
+++ Projekte/CryptoPals/src/set1/Challenge4.java
@@ -0,0 +1,30 @@
+package set1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Challenge4 {
+	public static void main(String[] args) {
+
+		List<String> results = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader("4.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				results.addAll(Challenge3.genPossibleDecrypts(line));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Collections.sort(results, new EnglishLangSort());
+		System.out.println(String.format("decrypt : %s", results.get(0)));
+	}
+}

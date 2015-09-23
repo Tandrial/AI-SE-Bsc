557216f517f45037a9b1d5e995e68ebfd625a569
Michael Krane <michael.krane@gmx.de>
2015-09-23 23:06:49 +0200

    CleanUp

--- Rechnerstrukturen_Betriebssysteme_WS1314/blatt_08/rsbs_ws1314_gruppe13/app2/Debug/board/board_xmega_a1_xplained.d
+++
@@ -1,9 +0,0 @@
-board/board_xmega_a1_xplained.d: ../board/board_xmega_a1_xplained.c \
- ../board/board_xmega_a1_xplained.h ../board/../mcu/mcu.h \
- ../board/../mcu/atmel/mcu_atxmega128a1.h
-
-../board/board_xmega_a1_xplained.h:
-
-../board/../mcu/mcu.h:
-
-../board/../mcu/atmel/mcu_atxmega128a1.h:
--- Rechnerstrukturen_Betriebssysteme_WS1314/blatt_08/rsbs_ws1314_gruppe13/app2/Debug/modul/fifo/fifo.d
+++
@@ -1,9 +0,0 @@
-modul/fifo/fifo.d: ../modul/fifo/fifo.c ../modul/fifo/fifo.h \
- ../modul/fifo/../../mcu/mcu.h \
- ../modul/fifo/../../mcu/atmel/mcu_atxmega128a1.h
-
-../modul/fifo/fifo.h:
-
-../modul/fifo/../../mcu/mcu.h:
-
-../modul/fifo/../../mcu/atmel/mcu_atxmega128a1.h:
